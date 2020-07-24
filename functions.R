8ed6360b3890ba6b06f63118025cc043afe8465b
Casey W Dunn <casey.dunn@yale.edu>
2020-07-24 06:16:30 -0400

    minor

--- functions.R
+++ functions.R
@@ -1,9 +1,9 @@
 # simple simulation under the jc model
-sim_jc = function( mu = 0.001, t = 100, n = 10000  ){
+sim_jc = function( mu = 0.001, t = 100, n = 10000, first =  sample( c('C', 'G', 'T', 'A') ) ){
   beta = mu / 4
   i = t/n  # time interval per step
   y = sample( c('C', 'G', 'T', 'A', NA), size=n, replace = TRUE, prob=c( beta, beta, beta, beta, 1-4 * beta  ) )
-  y[1] = sample( c('C', 'G', 'T', 'A') ) # initialize ancestral state
+  y[1] = first # initialize ancestral state
   
   t = which(!is.na(y))
   
