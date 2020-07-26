a0a47e1d2042bddc29e965428ffcbd0547972840
Casey W Dunn <casey.dunn@yale.edu>
2020-07-26 08:18:15 -0400

    simulation working, and text updated with sensible parameters

--- functions.R
+++ functions.R
@@ -1,5 +1,5 @@
 # simple simulation under the jc model
-sim_jc = function( mu = 0.001, t = 100, n = 1000, first =  NULL ){
+sim_jc = function( mu = 1, t = 100, n = 1000, first =  NULL ){
   
   if( is.null(first)){
     first = sample( c('C', 'G', 'T', 'A') )
@@ -7,17 +7,29 @@ sim_jc = function( mu = 0.001, t = 100, n = 1000, first =  NULL ){
   
   beta = mu / 4
   i = t/n  # time interval per step
-  p = beta * i
+  p = beta * i # expected change over the interval
   y = sample( c('C', 'G', 'T', 'A', NA), size=n, replace = TRUE, prob=c( p, p, p, p, (1 - 4 * p)  ) )
   y[1] = first # initialize ancestral state
   
   events = which(!is.na(y))
   
+  start = (events-1) * i
+  
+  # If there multiple events, the end times are just the start time of the next events. If there is a single 
+  # event, there are no future events with start times to consider.
+  end = c()
+  
+  if ( length(events) > 1 ){
+    end = start[2:length(events)]
+  }
+  
+  # need to add the end time of the last event, which is just the total time elapsed
+  end = c( end, t )
+  
   segments = tibble(
     nucleotide = y[events],
-    start = t * i,
-    end = c((events-1)[2:length(events)], length(y)) * i
-    
+    start = start,
+    end = end
   )
   
   segments
--- tests/testthat/test_general.R
+++ tests/testthat/test_general.R
@@ -1,7 +1,33 @@
+library(tidyverse)
 source( "../../functions.R" )
+
+epsilon = 0.1
 
 context("testing framework")
 
 test_that("Testing works", {
   expect_equal( 1, 1 )
+})
+
+context("simulate_jc")
+
+# run replicates simulations
+mu = 0.1
+t = 100
+replicates = 100
+x = lapply( rep("A", 500), function(x){sim_jc( mu = 0.1, t = 100, n = 1000, first =  x )} )
+
+replacements = lapply( x, nrow  ) %>% unlist() 
+replacements = replacements - 1 # The first replacement isn't really a replacement, it is just the initialization
+
+test_that("The number of replacements is close to mu*t", {
+  expect_lt( abs( mu * t - mean(replacements)), mu * t * epsilon )
+})
+
+test_that("There is a single entry if mu=0", {
+  expect_equal( 1, nrow( sim_jc(mu=0) )  )
+})
+
+test_that("There is a single entry if t=0", {
+  expect_equal( 1, nrow( sim_jc(t=0) )  )
 })
